dfdcf73ad1b6bf5f1f4425ed0bff416d1ff13ae2
alex <arodriguez202020@gmail.com>
2021-12-04 18:59:26 -0600

    change css styles and render button counter in index.js and empty div in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,7 @@
 import React from "react";
 
-var isDone = false;
-
-function strike() {
-  isDone = true;
-}
-
-function unStrike() {
-  isDone = false;
-}
-
 function App() {
-  return (
-    <div>
-      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
-      <button onClick={strike}>Change to strike through</button>
-      <button onClick={unStrike}>Change back</button>
-    </div>
-  );
+  return <div />;
 }
 
 export default App;
